f26db42cc9ce9df1668a18b2205ae819dc7f0e0c
Nour Ben Aouicha <92543024+Nour-ben-aouicha@users.noreply.github.com>
2025-01-22 20:40:53 +0100

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,110 +8,145 @@ import projImg5 from "../assets/img/yy.avif";
 import projImg6 from "../assets/img/h.webp";
 import projImg7 from "../assets/img/rr.png";
 import projImg8 from "../assets/img/emotion.jpg";
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-  const projectSections = [
-    [
-      {
-        title: "Ethereum Price Prediction",
-        description: "Models used: DRL, HMM & LSTM",
-        imgUrl: projImg1,
-        link: "https://medium.com/@sarraayed124/ethereum-price-prediction-model-comparison-e5fa0e7741b9",
-      },
-      {
-        title: "Risk Management Recommendation",
-        description: "Models used: Roberta Large, GNN, Rebel",
-        imgUrl: projImg2,
-        link: "https://github.com/Nour-ben-aouicha/Risk-Management-Recommendation",
-      },
-      {
-        title: "Time series stock prediction",
-        description: "Models used: ARIMA Models",
-        imgUrl: projImg3,
-        link: "https://github.com/Nour-ben-aouicha/Time-Series",
-      },
-    ],
-    [
-      {
-        title: "Mind Match: Career center platform",
-        description: "Techniques/models used: Web scraping, OpenAI API, content-based recommendation, KMeans, Random Forest, Logistic Regression",
-        imgUrl: projImg4,
-        link: "https://github.com/Nour-ben-aouicha/MindMatch_careerCenter_Models",
-      },
-      {
-        title: "YOGA Poses classification",
-        description: "Models used: CNN, EfficientNetB0",
-        imgUrl: projImg5,
-        link: "https://github.com/Nour-ben-aouicha/Yoga_Poses",
-      },
-      {
-        title: "Chronic Kidney Disease",
-        description: "Techniques/models used: AdaBoost, Naive Bayes, SVM, Random Forest",
-        imgUrl: projImg6,
-        link: "https://github.com/Nour-ben-aouicha/chronic_Kidney_Disease",
-      },
-    ],
-    [
-      {
-        title: "Raisin Variety Prediction",
-        description: "Models used: Linear Regression",
-        imgUrl: projImg7,
-        link: "https://github.com/Nour-ben-aouicha/Raisin_Variety_Classification",
-      },
-      {
-        title: "Emotion Detection",
-        description: "Models used: CNN custom, VGG, ResNet",
-        imgUrl: projImg8,
-        link: "https://github.com/Nour-ben-aouicha/Emotion_Detection",
-      },
-    ],
+  const projects = [
+    {
+      title: "Ethereum Price Prediction",
+      description: "Models used: DRL, HMM & LSTM",
+      imgUrl: projImg1,
+      link: "https://medium.com/@sarraayed124/ethereum-price-prediction-model-comparison-e5fa0e7741b9",
+    },
+    {
+      title: "Risk Management Recommendation",
+      description: "Models used: Roberta Large, GNN, Rebel",
+      imgUrl: projImg2,
+      link: "https://github.com/Nour-ben-aouicha/Risk-Management-Recommendation",
+    },
+    {
+      title: "Time series stock prediction",
+      description: "Models used: ARIMA Models",
+      imgUrl: projImg3,
+      link: "https://github.com/Nour-ben-aouicha/Time-Series",
+    },
+  ];
+
+  const projects2 = [
+    {
+      title: "Mind Match: Career center platform",
+      description:
+        "techniques/models used: web scraping, OpenAI API, content-based Recommendation system, KMeans clustering, Random Forest, Logistic Regression",
+      imgUrl: projImg4,
+      link: "https://github.com/Nour-ben-aouicha/MindMatch_careerCenter_Models",
+    },
+    {
+      title: "YOGA Poses classification",
+      description: "Models used: CNN, EfficientNetB0",
+      imgUrl: projImg5,
+      link: "https://github.com/Nour-ben-aouicha/Yoga_Poses",
+    },
+    {
+      title: "Chronic Kidney Disease",
+      description:
+        "Techniques/Models used: Feature selection, AdaBoost, Naive Bayes, SVM, KNN, Random Forest, Decision Tree",
+      imgUrl: projImg6,
+      link: "https://github.com/Nour-ben-aouicha/chronic_Kidney_Disease",
+    },
+  ];
+
+  const projects3 = [
+    {
+      title: "Raisin Variety Prediction",
+      description: "Models used: linear Regression",
+      imgUrl: projImg7,
+      link: "https://github.com/Nour-ben-aouicha/Raisin_Variety_Classification",
+    },
+    {
+      title: "Emotion Detection",
+      description: "Models used: CNN custom, VGG, ResNet",
+      imgUrl: projImg8,
+      link: "https://github.com/Nour-ben-aouicha/Emotion_Detection",
+    },
   ];
 
   return (
     <section className="project" id="projects">
-      <Container>
+      <Container style={{ marginBottom: "0px" }}>
         <Row>
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) => (
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <div
+                  className={isVisible ? "animate__animated animate__fadeIn" : ""}
+                >
                   <h2>Projects</h2>
                   <p>
-                    This section explores projects from my GitHub repository. Visit via this{" "}
+                    This section will offer a deeper exploration of the projects
+                    featured on my GitHub repository. You can access these
+                    projects by following this{" "}
                     <a
                       href="https://github.com/Nour-ben-aouicha"
                       style={{ color: "white", textDecoration: "underline" }}
                     >
                       link
                     </a>
-                    . Each project includes details about its purpose and implementation.
+                    . Each project is meticulously detailed below, offering an
+                    in-depth explanation of its scope, purpose, and
+                    implementation.
                   </p>
-                  <Tab.Container id="projects-tabs" defaultActiveKey="0">
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav
                       variant="pills"
                       className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
                     >
-                      {["Section 1", "Section 2", "Section 3"].map((section, idx) => (
-                        <Nav.Item key={idx}>
-                          <Nav.Link eventKey={`${idx}`} style={{ cursor: "pointer" }}>
-                            {section}
-                          </Nav.Link>
-                        </Nav.Item>
-                      ))}
+                      <Nav.Item>
+                        <Nav.Link eventKey="first" style={{ cursor: "pointer" }}>
+                          Section 1
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second" style={{ cursor: "pointer" }}>
+                          Section 2
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third" style={{ cursor: "pointer" }}>
+                          Section 3
+                        </Nav.Link>
+                      </Nav.Item>
                     </Nav>
-                    <Tab.Content>
-                      {projectSections.map((projects, idx) => (
-                        <Tab.Pane eventKey={`${idx}`} key={idx}>
-                          <Row>
-                            {projects.map((project, index) => (
-                              <ProjectCard key={index} {...project} />
-                            ))}
-                          </Row>
-                        </Tab.Pane>
-                      ))}
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => (
+                            <ProjectCard key={index} {...project} />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {projects2.map((project, index) => (
+                            <ProjectCard key={index} {...project} />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {projects3.map((project, index) => (
+                            <ProjectCard key={index} {...project} />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
                 </div>
@@ -120,7 +155,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} alt="Background" />
+      <img className="background-image-right" src={colorSharp2} alt="background" />
     </section>
   );
 };
